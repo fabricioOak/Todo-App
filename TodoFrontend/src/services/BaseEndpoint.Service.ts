fea6eea0d119e43ee7b57e08a8eeff28bb7f18ae
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-07-08 20:35:06 -0300

    feat(TodoFrontend): add BaseEndpoint.Service and Todo.Service to handle API endpoints
    - Added `BaseEndpoint.Service.ts` to define the base URL for API endpoints.
    - Added `Todo.Service.ts` to define the endpoints for CRUD operations on todos.
    
    These services will be used to make API calls in the TodoFrontend application.

---
+++ TodoFrontend/src/services/BaseEndpoint.Service.ts
@@ -0,0 +1,5 @@
+const BaseEndpoint = {
+  BaseUrl:  'http://localhost:5053/'
+}
+
+export default BaseEndpoint
---
+++ TodoFrontend/src/services/Todo.Service.ts
@@ -0,0 +1,11 @@
+import BaseEndpoint from "./BaseEndpoint.Service"
+
+const TodoServices = {
+  GetMany: BaseEndpoint.BaseUrl + 'v1/todos',
+  GetOne: (id: number) => BaseEndpoint.BaseUrl + `v1/todos/${id}`,
+  Create: BaseEndpoint.BaseUrl + 'v1/todos',
+  Update: (id: number) => BaseEndpoint.BaseUrl + `v1/todos/${id}`,
+  Delete: (id: number) => BaseEndpoint.BaseUrl + `v1/todos/${id}`
+}
+
+export default TodoServices
