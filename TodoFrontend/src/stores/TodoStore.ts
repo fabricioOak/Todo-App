8a4e077326ef662cc7d6cd9fa78eeeaf9e0ef5d0
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-07-08 20:43:58 -0300

    feat(TodoStore.ts): add TodoStore to handle CRUD operations for todos
    The TodoStore file is added to the TodoFrontend/src/stores directory. This store is responsible for handling CRUD operations for todos. It defines the following interfaces: Todo and TodoCreation. The Todo interface represents the structure of a todo item, including its id, title, content, and done status. The TodoCreation interface represents the structure of a new todo item, including its title and content.
    
    The useTodoStore function is exported from the file using the defineStore function from the pinia library. This function defines the actions that can be performed on the todo store. The actions include:
    
    - getMany: Fetches multiple todos from the server using the TodoServices.GetMany endpoint.
    - getOne: Fetches a single todo by its id from the server using the TodoServices.GetOne endpoint.
    - create: Creates a new todo on the server using the TodoServices.Create endpoint.
    - updateState: Updates the done status of a todo on the server using the TodoServices.Update endpoint.
    - delete: Deletes a todo from the server using the TodoServices.Delete endpoint.
    
    Each action makes an HTTP request to the server using the fetch function. If the request is not successful (response.ok is false), an error is thrown. Otherwise, the response data is returned or the action is completed.
    
    The TodoServices module is imported from '@/services/Todo.Service' to provide the endpoints for the todo API.

---
+++ TodoFrontend/src/stores/TodoStore.ts
@@ -0,0 +1,62 @@
+import { defineStore } from 'pinia'
+import TodoServices from '@/services/Todo.Service'
+
+interface Todo {
+  id: number
+  title: string
+  content: string
+  done : boolean
+}
+
+interface TodoCreation {
+  title: string
+  content: string
+}
+
+export const useTodoStore = defineStore('todo', {
+  actions: {
+    async getMany(): Promise<Todo[]> {
+      const response = await fetch(TodoServices.GetMany);
+      if (!response.ok) {
+        throw new Error('Failed to fetch todos');
+      }
+      const data = await response.json();
+      return data;
+    },
+
+    async getOne(id: number): Promise<Todo> {
+      const response = await fetch(TodoServices.GetOne(id));
+      if (!response.ok) {
+        throw new Error('Failed to fetch todo');
+      }
+      const data = await response.json();
+      return data;
+    },
+
+    async create(model: TodoCreation) {
+      const newTodo = {
+        title: model.title,
+        content: model.content,
+      };
+      await fetch(TodoServices.Create, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newTodo),
+      });
+    },
+
+    async updateState(id: number) {
+      await fetch(TodoServices.Update(id), {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+      })
+    },
+
+    async delete(id: number) {
+      await fetch(TodoServices.Delete(id), {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      })
+    },
+  },
+});
