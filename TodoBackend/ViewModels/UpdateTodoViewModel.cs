14429e52eeb508d8d358d1c1fe4534524e81c319
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-07-08 19:18:48 -0300

    chore(UpdateTodoViewModel.cs): remove unused properties Title and Content to improve code cleanliness and remove unnecessary code

--- TodoBackend/ViewModels/UpdateTodoViewModel.cs
+++ TodoBackend/ViewModels/UpdateTodoViewModel.cs
@@ -2,8 +2,6 @@ namespace TodoBackend.ViewModels
 {
   public class UpdateTodoViewModel
   {
-    public required string Title { get; set; }
-    public required string Content { get; set; }
     public bool Done { get; set; }
   }
 }
