927388175373be51eca67a807513e19dd3aca652
Fabricio Carvalho <fcarvalho1999@gmail.com>
2023-07-09 15:21:48 -0300

    fix(App.vue): fix image source path for favicon and add alt text for accessibility feat(App.vue): add functionality to clear all todos by adding a deleteAll method and a Clear button

--- TodoFrontend/src/App.vue
+++ TodoFrontend/src/App.vue
@@ -1,38 +1,30 @@
 <template>
-  <div v-if="!todos">Loading...</div>
-  <div v-else class="h-screen">
-    <header class="pr-4 pt-4 text-right w-full">
-      <p class="text-xl text-gray-400">
-        Made by
-        <a class="text-gray-300" target="__blank" href="https://github.com/fabricioOak">fabricioOak</a>
-      </p>
-    </header>
+  <div>
     <main class="transition-all duration-200 w-full flex flex-col items-center pt-16" id="app">
       <div>
-        <img src="../public/favicon.svg" alt="" />
+        <img class="mx-auto" src="/todo-app.ico" alt="" />
         <h1 class="text-4xl capitalize mb-2">To do list</h1>
       </div>
       <p class="mb-8">What do you need to do?</p>
-      <div class="flex flex-col justify-center items-center max-w-2xl sm:flex-row">
-
-        <div class="w-80">
+      <div class="flex flex-col justify-center items-center sm:max-w-2xl ">
+        <div class="flex flex-col w-full">
           <input
-            class="focus:placeholder-transparent mb-2 w-80 border-white bg-gray-800 text-center rounded text-white text-2xl py-2 m-0 focus:outline-none"
+            class="focus:placeholder-transparent mb-2 w-80 sm:mx-0 border-white bg-gray-800 text-center rounded text-white text-2xl py-2 focus:outline-none"
             type="text" v-model="todoTitle" placeholder="Add a title" />
           <textarea
-            class="focus:placeholder-transparent w-80 border-white bg-gray-800 text-center rounded text-white text-2xl py-2 m-0 focus:outline-none"
+            class="focus:placeholder-transparent w-80 sm:mx-0 border-white bg-gray-800 text-center rounded text-white text-2xl py-2 focus:outline-none"
             type="text" v-model="todoContent" placeholder="Add the content" />
         </div>
 
         <div class="flex">
-          <button @click="addTodo" @keyup.enter="addTodo"
-            class="transition-all duration-200 button sm:mt-0 mt-4 text-white hover:bg-green-600 bg-green-400">
+          <button @click="addTodo"
+            class="transition-all duration-200 button mt-4 text-white hover:bg-green-600 bg-green-400 mr-2">
             Add
           </button>
-          <!-- <button
-            class="transition-all w-full duration-200 button sm:mt-0 mt-4 text-white hover:bg-yellow-600 bg- bg-yellow-500">
-            Remove all
-          </button> -->
+          <button @click="deleteAll"
+            class="transition-all duration-200 button mt-4 text-white hover:bg-yellow-600 bg-yellow-400">
+            Clear
+          </button>
 
         </div>
       </div>
@@ -58,6 +50,7 @@ interface Todo {
 
 const todoTitle = ref("");
 const todoContent = ref("");
+const newTodo = ref("");
 const store = useTodoStore();
 const todos = ref<Todo[]>([]);
 
@@ -98,11 +91,18 @@ const deleteTodo = async (id: number) => {
   )
 }
 
-const newTodo = ref("");
+const deleteAll = async () => {
+  await store.deleteAll()
+
+  await store.getMany().then(
+    response => todos.value = response
+  )
+}
+
 </script>
 
 <style lang="postcss">
 .button {
-  @apply appearance-none border-none outline-none font-bold text-black text-2xl py-2 px-4 rounded-lg ml-2 cursor-pointer;
+  @apply appearance-none border-none outline-none font-bold text-black text-2xl py-2 px-4 rounded-lg cursor-pointer;
 }
 </style>
